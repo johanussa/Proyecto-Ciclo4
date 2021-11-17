1a69819ff742c8cc52ee2851abc3bcf5444aef09
Johan Ussa Rubio <johanuss0405@gmail.com>
2021-11-16 23:21:42 -0500

    Se realiza el model de los proyectos a guardar en DB

--- AppProject4/AppProject4-Backend/index.js
+++ AppProject4/AppProject4-Backend/index.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const app = require('./app');
-const DataBase = require('./src/database/DataBase');
+import mongoose from 'mongoose';
+import app from './app';
+import DataBase from './src/database/DataBase';
 
 mongoose.connect(DataBase.db, {     
     useNewUrlParser: true,
@@ -9,6 +9,6 @@ mongoose.connect(DataBase.db, {
     .then( () => console.log("La Base de datos se ha Conectado Correctamente"))    
     .catch(error => console.log("Error de Conexion ", error));
 
-    app.listen(DataBase.port, () => {
-        console.log(`Api Rest corriendo en el puerto #${ DataBase.port }`);
-    });
+app.listen(DataBase.port, () => {
+    console.log(`Api Rest corriendo en el puerto #${ DataBase.port }`);
+});
