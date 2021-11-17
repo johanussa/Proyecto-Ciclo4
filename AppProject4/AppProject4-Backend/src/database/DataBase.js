d544c321185b5bf29bfcbb5c562b5e50e3de4853
Johan Ussa Rubio <johanuss0405@gmail.com>
2021-11-17 13:54:09 -0500

    Correr api con nodemon

--- AppProject4/AppProject4-Backend/src/database/DataBase.js
+++ AppProject4/AppProject4-Backend/src/database/DataBase.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 3001,
+    port: process.env.PORT || 3000,
     db: process.env.MONGODB || "mongodb+srv://adminApp:0000@vitafamilydb.iq2cq.mongodb."
        + "net/AppProject?retryWrites=true&w=majority"
 };
